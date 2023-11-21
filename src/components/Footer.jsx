20fc3002202d188670ce7d869eb0263ca6e758e1
Drey <dareybee@gmail.com>
2023-11-20 20:23:39 -0500

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
 const Footer = () => {
   return (
     <footer className="h-auto w-full bg-gradient-to-r from-[#626a71] to-[#2a3543]  text-white py-10 px-32 flex items-center justify-between ">
-      <div className=" w-1/3 ">
+      <div className=" w-1/3 m-auto ">
         <a href="#"><img src="./logo.svg" alt="" /></a>
       </div>
-      <div className=" w-1/3 m-auto flex items-center">
-        <p className=" font-bold">© 2023 Kascore – All Rights Reserved.</p>
+      <div className=" w-1/3 text-center ">
+        <p className=" font-bold ">© 2023 Kascore – All Rights Reserved.</p>
       </div>
       <div className=" w-[20%] flex items-center justify-between">
         <div className=" w-1/4 ">
