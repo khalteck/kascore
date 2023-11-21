00d36df60dc405a12e0bda6456d4ce80f860b14b
Drey <dareybee@gmail.com>
2023-11-21 09:40:27 -0500

    footerr

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,25 +1,32 @@
 const Footer = () => {
   return (
-    <footer className="h-auto w-full bg-gradient-to-r from-[#626a71] to-[#2a3543]  text-white py-10 px-32 flex items-center justify-between ">
-      <div className=" w-1/3 m-auto ">
-        <a href="#"><img src="./logo.svg" alt="" /></a>
+    <footer className="h-auto w-full block bg-gradient-to-r from-[#5a636b] to-[#2a3543] text-white py-5 px-5 lg:py-10 lg:px-32 lg:flex items-center justify-between ">
+
+      <div className=" w-full lg:w-1/3 my-10 lg:my-0 ">
+        <a href="#"><img src="./logo.svg" className=" m-auto lg:m-0" alt="" /></a>
       </div>
-      <div className=" w-1/3 text-center ">
-        <p className=" font-bold ">© 2023 Kascore – All Rights Reserved.</p>
+
+
+      <div className=" w-full lg:w-1/3 text-center lg:text-left mb-10 lg:mb-0 ">
+        <p className=" font-bold text-[15px] ">© 2023 Kascore – All Rights Reserved.</p> 
       </div>
-      <div className=" w-[20%] flex items-center justify-between">
-        <div className=" w-1/4 ">
-          <a href="#"><img src="./icon-facebook.svg" alt="" /></a>
+
+
+      <div className=" w-4/5 lg:w-[20%] mx-auto my-10 lg:mx-0 lg:my-0 flex items-center justify-between">
+
+        <div className=" w-[15%] lg:w-1/4 ">
+          <a href="#"><img src="./icon-facebook.svg" className=" m-auto lg:m-0 " alt="" /></a>
         </div>
-        <div className=" w-1/4 ">
-          <a href="#"><img src="./icon-instagram.svg" alt="" /></a>
+        <div className=" w-[15%] lg:w-1/4 ">
+          <a href="#"><img src="./icon-instagram.svg" className=" m-auto lg:m-0" alt="" /></a>
         </div>
-        <div className=" w-1/4 ">
-          <a href="#"><img src="./icon-twitter.svg" alt="" /></a>
+        <div className=" w-[15%] lg:w-1/4 ">
+          <a href="#"><img src="./icon-twitter.svg" className=" m-auto lg:m-0" alt="" /></a>
         </div>
-        <div className=" w-1/4  ">
-          <a href="#"><img src="./icon-youtube.svg" alt="" /> </a>
+        <div className=" w-[15%] lg:w-1/4 ">
+          <a href="#"><img src="./icon-youtube.svg" className=" m-auto lg:m-0" alt="" /> </a>
         </div> 
+
       </div>
       
     </footer>
