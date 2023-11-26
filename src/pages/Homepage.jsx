ff8e6040d5044afba6b7f0511f49605564e24343
Drey <dareybee@gmail.com>
2023-11-26 08:16:17 -0500

    home

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -8,11 +8,46 @@ const Homepage = () => {
       <div className="w-full pt-[130px] md:pt-[170px] pb-12 px-5 lg:px-[12%] h-[2000px] bg-[#121a20] text-white">
         <div className="w-full h-full flex gap-8">
           <div className="w-[25%] bg-[#1d2732] h-[70%] rounded-lg hidden md:flex flex-col">
-            <div>Score</div>
-            <div>Score</div>
-            <div>Score</div>
-            <div>Score</div>
-            <div>Score</div>
+            <div className=" flex items-center gap-7 mx-5 my-2 cursor-pointer hover ">
+              <img src="/images/icons8-england-48.png" alt="" className=" w-6 h-5" />
+              <p className=" text-base">England</p>
+            </div>
+            <div className=" flex items-center gap-7 mx-5 mb-3 cursor-pointer hover ">
+              <img src="/images/icons8-england-48.png" alt="" className=" w-6 h-5" />
+              <p className=" text-base">England</p>
+            </div>
+            <div className=" flex items-center gap-7 mx-5 mb-3 cursor-pointer hover ">
+              <img src="/images/icons8-england-48.png" alt="" className=" w-6 h-5" />
+              <p className=" text-base">England</p>
+            </div>
+            <div className=" flex items-center gap-7 mx-5 mb-3 cursor-pointer hover ">
+              <img src="/images/icons8-england-48.png" alt="" className=" w-6 h-5" />
+              <p className=" text-base">England</p>
+            </div>
+            <div className=" flex items-center gap-7 mx-5 mb-3 cursor-pointer hover ">
+              <img src="/images/icons8-england-48.png" alt="" className=" w-6 h-5" />
+              <p className=" text-base">England</p>
+            </div>
+            <div className=" flex items-center gap-7 mx-5 mb-3 cursor-pointer hover ">
+              <img src="/images/icons8-england-48.png" alt="" className=" w-6 h-5" />
+              <p className=" text-base">England</p>
+            </div>
+            <div className=" flex items-center gap-7 mx-5 mb-3 cursor-pointer hover ">
+              <img src="/images/icons8-england-48.png" alt="" className=" w-6 h-5" />
+              <p className=" text-base">England</p>
+            </div>
+            <div className=" flex items-center gap-7 mx-5 mb-3 cursor-pointer hover ">
+              <img src="/images/icons8-england-48.png" alt="" className=" w-6 h-5" />
+              <p className=" text-base">England</p>
+            </div>
+            <div className=" flex items-center gap-7 mx-5 mb-3 cursor-pointer hover ">
+              <img src="/images/icons8-england-48.png" alt="" className=" w-6 h-5" />
+              <p className=" text-base">England</p>
+            </div>
+            <div className=" flex items-center gap-7 mx-5 mb-3 cursor-pointer hover ">
+              <img src="/images/icons8-england-48.png" alt="" className=" w-6 h-5" />
+              <p className=" text-base">England</p>
+            </div>
           </div>
 
           <div className="w-full md:w-[50%] bg-[#1d2732] h-full rounded-lg p-3">
