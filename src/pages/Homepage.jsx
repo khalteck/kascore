3af2bb03167fa0f54d01abae3753b566cd43599b
Drey <dareybee@gmail.com>
2023-12-17 19:41:36 -0500

    news

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -33,15 +33,36 @@ const Homepage = () => {
             </div>
           </div>
           <div className="w-[30%] min-w-[200px] bg-[#1d2732] h-auto rounded-lg hidden lg:block p-4 ">
-            <div className=" w-full bg-[#1d2732] h-[400px] ">
+            <div className=" w-full bg-[#1d2732] h-[700px] ">
               <div className=" flex items-center justify-between md:border md:border-neutral-100/20 cursor-pointer py-2 px-2 mb-4 rounded-lg ">
                 <p className=" text-[1.2rem] font-bold ">Featured News</p>
                 <img src="/images/icons8-right-arrow-30.png" alt="" className=" w-5 h-5 " />
               </div>
-              <div className=" w-full ">
-                <img src="/images/K1.jpg" alt="" className=" w-full h-[200px] rounded-lg " />
-                <h1>Will be back soon!</h1>
+              <div className=" w-full h-auto relative border border-neutral-100/20 rounded-lg ">
+                <div className=" w-full h-auto opacity-90">
+                  <img src="/images/fayas.jpg" alt="" className=" w-full h-[200px] rounded-lg  " />
+                </div>
+                <div className=" w-full h-[200px] opacity-90 p-3 absolute top-0 left-0 ">
+                  <p className=" w-[85%] mt-[130px] text-left font-bold text-white text-[1rem] ">Five talking points as Liverpool are held to United stalemate</p>
+                </div>
               </div>
+              <div className=" w-full h-auto relative border border-neutral-100/20 my-4 rounded-lg ">
+                <div className=" w-full h-auto opacity-90">
+                  <img src="/images/sven.jpg" alt="" className=" w-full h-[200px] rounded-lg  " />
+                </div>
+                <div className=" w-full h-[200px] opacity-90 p-3 absolute top-0 left-0 ">
+                  <p className=" w-[85%] mt-[130px] text-left font-bold text-white text-[1rem] ">Five talking points as Liverpool are held to United stalemate</p>
+                </div>
+              </div>
+              <div className=" w-full h-auto relative border border-neutral-100/20 rounded-lg ">
+                <div className=" w-full h-auto opacity-90 ">
+                  <img src="/images/vienna.jpg" alt="" className=" w-full h-[200px] rounded-lg  " />
+                </div>
+                <div className=" w-full h-[200px] opacity-90 p-3 absolute top-0 left-0 ">
+                  <p className=" w-[85%] mt-[130px] text-left font-bold text-white text-[1rem] ">Five talking points as Liverpool are held to United stalemate</p>
+                </div>
+              </div>
+             
             </div> 
             <div className="w-full bg-inherit h-[200px] mt-5 ">
               <h1 className="text-[.75rem]">ADVERTISEMENT</h1>
